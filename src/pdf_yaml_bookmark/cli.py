97907c25ff7161bedbd781b5746c871ee0939084
Yuta  Yamamoto <yutayamamoto@Yutos-MacBook.local>
2025-03-01 13:16:13 +0900

    inrtoduce typer for command line interface

--- src/pdf_yaml_bookmark/cli.py
+++ src/pdf_yaml_bookmark/cli.py
@@ -1,49 +1,51 @@
 import argparse
 import os
+from pathlib import Path
 import re
 import sys
+
+import typer
 
 import pdf_yaml_bookmark.common as common
 import pdf_yaml_bookmark.run_gs as run_gs
 import pdf_yaml_bookmark.bkm_to_yaml as bkm_to_yaml
 import pdf_yaml_bookmark.yaml_to_gs as yaml_to_gs
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="pdfoutline.py",
-        description='Adds bookmark (table of contents) to a PDF document.',
-        epilog="Example bookmark file at https://github.com/yutayamamoto/pdfoutline")
-    parser.add_argument('in_pdf', metavar = 'pdf_in', help = 'Input pdf file')
-    parser.add_argument('in_bkm', metavar = 'bkm_in', help = 'Table of contents file in the specified format')
-    parser.add_argument('-o', '--output', required = True, metavar = 'pdf_out', help = 'Output pdf file')
-    parser.add_argument('-g', '--gs-path', type=str, help = "Path to ghostscript executable", default='gs')
-    parser.add_argument('-f', '--format', type=str, help = 'A format of bookmark file ["bkm","yaml"]')
-    parser.add_argument('-e', '--export-yaml', type=str, help = "Path to export bookmark file in YAML format")
-    parser.add_argument('-s', '--show-progress', action = 'store_true', help = "Show progress bar")
-    args = parser.parse_args()
+app = typer.Typer()
 
-    if args.in_pdf == args.output:
+@app.command()
+def cli(
+    input_pdf: Path = typer.Argument(..., help="Input PDF file"),
+    bookmark_file: Path = typer.Option(..., "-b", "--bookmark", help="Bookmark file (.bkm or .yaml/.yml)"),
+    output_pdf: Path = typer.Option(..., "-o", "--output", help="Output PDF file"),
+    export_yaml: Path = typer.Option(None, "--export-yaml", help="Export intermediate YAML"),
+    show_progress: bool = typer.Option(False, "--show-progress", help="Show progress bar"),
+    gs_path: Path = typer.Option("gs", "--gs-path", help="A path to ghostscript executable")
+):
+    if input_pdf == output_pdf:
         common.eprint('Error: specify different names for input and output files.')
         sys.exit(-1)
 
-    bkm_filename = args.in_bkm
-    if not common.check_readability(bkm_filename):
+    if not common.check_readability(bookmark_file):
         sys.exit(-1)
 
-    # Read bookmark in YAML format
-    if args.format == "yaml":
-        with open(bkm_filename) as f:
+    # Identify the format of the bookmark file
+    if bookmark_file.suffix in [".yaml", ".yml"]:
+        with open(bookmark_file) as f:
             yaml = f.read()
     else:
-        with open(bkm_filename) as f:
+        with open(bookmark_file) as f:
             bkm = f.read()
             yaml = bkm_to_yaml.bkm_to_yaml(bkm)
+        if export_yaml:
+            with open(export_yaml, "w") as f:
+                f.write(yaml)
 
     # Create GS script
     gs = yaml_to_gs.yaml_to_gs(yaml)
 
     # Run GS
-    run_gs.run_gs(args.in_pdf, gs, args.output, gs_path=args.gs_path, show_progress=args.show_progress)
+    run_gs.run_gs(input_pdf, gs, output_pdf, gs_path=gs_path, show_progress=show_progress)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app()
